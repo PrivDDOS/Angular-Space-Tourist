f303149dfd8f625687a798a8ca006cbd7b343dfb
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-02-10 11:12:13 +0700

    Finish the destination page layout

--- src/app/components/destination/destination.ts
+++ src/app/components/destination/destination.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-destination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './destination.html',
   styleUrl: './destination.css',
 })
